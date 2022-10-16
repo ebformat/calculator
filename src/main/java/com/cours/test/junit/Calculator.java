b722771116ecca8503da233e91026091e2572598
ebevazaha <ebevazaha@ITEM-S136584.emea.msad.sopra>
2022-10-16 10:28:55 +0200

    1 er commit

---
+++ src/main/java/com/cours/test/junit/Calculator.java
@@ -0,0 +1,10 @@
+package com.cours.test.junit;
+
+public class Calculator {
+
+	public int addTwoNumber(int i, int j) {
+		// TODO Auto-generated method stub
+		return i + j;
+	}
+
+}
---
+++ src/test/java/com/cours/test/junit/testing/CalculatorTest.java
@@ -0,0 +1,25 @@
+package com.cours.test.junit.testing;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+import com.cours.test.junit.Calculator;
+
+class CalculatorTest {
+
+	@Test
+	void testAddTwoPositiveNumber() {
+
+		// Given
+		int a = 2;
+		int b = 3;
+		Calculator cal = new Calculator();
+		// WHEN
+		int c = cal.addTwoNumber(2, 3);
+		// THEN
+		assertThat(c).isEqualTo(5);
+
+	}
+
+}
